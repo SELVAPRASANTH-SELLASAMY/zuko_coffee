d505ca109442cc945fa370f2fa0b74a761bcf0ac
prasanthsamy <selvaprasanths.20mech@kongu.edu>
2024-07-26 21:35:09 +0530

    initial commit

--- src/components/reviews/ReviewTile.jsx
+++ src/components/reviews/ReviewTile.jsx
@@ -1,13 +1,14 @@
-import { useMemo, useRef } from 'react';
+import { useMemo } from 'react';
 import reviewStyle from './reviews.module.css';
 function ReviewTile({customerReviewArray}){
-    const ratingContainer = useRef();
     const {rating} = customerReviewArray;
     const stars = useMemo(() => {
-        const ratingStar = <span className={reviewStyle.star}>&#9733;</span>;
-        const unRatingStar = <span className={reviewStyle.star}>&#9734;</span>;
-        const rated = Array(Number(rating)).fill(ratingStar);
-        const unRated = Array(5 - Number(rating)).fill(unRatingStar);
+        const rated = Array.from({length:Number(rating)},(_,index)=>(
+            <span key={`rated-${index}`} className={reviewStyle.star}>&#9733;</span>
+        ));
+        const unRated = Array.from({length:(5 - Number(rating))},(_,index)=>(
+            <span key={`unrated-${index}`} className={reviewStyle.star}>&#9734;</span>
+        ));
         return [...rated,...unRated];
     },[rating]);
 
@@ -17,7 +18,7 @@ function ReviewTile({customerReviewArray}){
             <img src={customerReviewArray.image} alt="avatar" />
             <p className={reviewStyle.name}>{customerReviewArray.clientName}</p>
             <p className={reviewStyle.designation}>{customerReviewArray.designation}</p>
-            <p ref={ratingContainer} className={reviewStyle.rating}>{stars}</p>
+            <p className={reviewStyle.rating}>{stars}</p>
             <p className={reviewStyle.feedback}>{customerReviewArray.feedback}</p>
         </div>
         </>
