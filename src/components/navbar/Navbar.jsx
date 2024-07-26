92f27776ac2238ea78aac15779613b24d929e8d3
prasanthsamy <selvaprasanths.20mech@kongu.edu>
2024-07-26 21:03:09 +0530

    initial commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,15 +2,27 @@ import NavStyle from "./navbar.module.css";
 import logo from '../../assets/logo.svg';
 import profileIcon from '../../assets/user_profile.svg';
 import cartIcon from '../../assets/cart.svg';
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 const Navbar = () =>{
     const navBar = useRef();
     const headerElement = useRef();
+    const [headerColor, setHeaderColor] = useState('transparent');
     useEffect(()=>{
         const navBarWidth = parseInt(getComputedStyle(navBar.current).width) + parseInt(getComputedStyle(document.querySelector('header')).paddingRight) * 2;
         document.querySelector('body').setAttribute('style',`--navbarWidth:${navBarWidth}`);
-        headerElement.current.style.background = `linear-gradient(to right, transparent calc(100% - ${navBarWidth}px),#7C573C ${navBarWidth}px)`;
+        headerElement.current.style.background = `linear-gradient(to right, ${headerColor} calc(100% - ${navBarWidth}px),#7C573C ${navBarWidth}px)`;
+    },[headerColor]);
+
+    useEffect(()=>{
+        const handleColorChange = () => {
+            window.scrollY >= 80 ? setHeaderColor('#1A1011') : setHeaderColor('transparent');
+        }
+        window.addEventListener('scroll',handleColorChange);
+        return ()=>{
+            window.removeEventListener('scroll',handleColorChange);
+        }
     },[]);
+
     return(
         <>
         <header ref={headerElement}>
