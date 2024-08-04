ec15a1a0a2e7daac62fe6921d1d6dca6104238d1
prasanthsamy <selvaprasanths.20mech@kongu.edu>
2024-08-04 10:11:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { Navbar, Hero, OurStory, Services, Offer, ReserveTable, Recommended, Men
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 function App() {
   return (
-    <HelmetProvider>
       <div className="App">
-        <Helmet>
-          <title>Zuko Coffee</title>
-          <meta name='description' content="Eliza is an online coffee store that offers the widest selection of specialty coffees and teas from around the world. From medium-dark roast single origin to flavored espresso beans, they offer a variety of ethically sourced products to tantalize any customer’s palate. For those looking for unique brewing equipment, Eliza also carries a full range of quality espresso makers, grinders, brewers, French presses and more. On top of all that, their baristas are highly trained professionals who know exactly how to make the perfect cup for each customer. Whether it's for home or business use, Eliza has you covered - all conveniently available at the click of a button!"/>
-          <meta name='keywords' content="zuko coffee, UI development, ReactJS, Selvaprasanth's project, landing page for coffee shop"/>
-          <meta name='author' content='Selvaprasanth'/>
-        </Helmet>
+        <HelmetProvider>
+          <Helmet>
+            <title>Zuko Coffee</title>
+            <meta name='description' content="Eliza is an online coffee store that offers the widest selection of specialty coffees and teas from around the world. From medium-dark roast single origin to flavored espresso beans, they offer a variety of ethically sourced products to tantalize any customer’s palate. For those looking for unique brewing equipment, Eliza also carries a full range of quality espresso makers, grinders, brewers, French presses and more. On top of all that, their baristas are highly trained professionals who know exactly how to make the perfect cup for each customer. Whether it's for home or business use, Eliza has you covered - all conveniently available at the click of a button!"/>
+            <meta name='keywords' content="zuko coffee, UI development, ReactJS, Selvaprasanth's project, landing page for coffee shop"/>
+            <meta name='author' content='Selvaprasanth'/>
+          </Helmet>
+        </HelmetProvider>
         <Navbar/>
         <main>
           <Hero/>
@@ -26,7 +27,6 @@ function App() {
         </main>
         <Footer/>
       </div>
-    </HelmetProvider>
   );
 }
 
--- src/components/ourStory/OurStory.jsx
+++ src/components/ourStory/OurStory.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import ourStoryStyle from './ourstory.module.css';
 import StoryFrame from './StoryFrame';
 function OurStory(){
@@ -15,17 +16,41 @@ function OurStory(){
             content:"Zuko is an online coffee store that offers the widest selection of specialty coffees and teas from around the world. From medium-dark roast single origin to flavored espresso beans, they offer a variety of ethically sourced products to tantalize any customer’s palate. For those looking for unique brewing equipment, Eliza also carries a full range of quality espresso makers, grinders, brewers, French presses and more. On top of all that, their baristas are highly trained professionals who know exactly how to make the perfect cup for each customer. Whether it's for home or business use, Eliza has you covered - all conveniently available at the click of a button!"
         }
     ];
-
+    const pageIndicator = () => {
+        const indicator = Array.from({length:contentArray.length},(_,index)=>(
+            <div key={`page${index}`} className={ourStoryStyle.indicationCircle}></div>
+        ));
+        return [...indicator];
+    }
+    const storyContainer = useRef();
+    const pagesContainer = useRef();
+    useEffect(()=>{
+        const storyContainerCopy = storyContainer.current;
+        var currentPage = 0;
+        const pages = pagesContainer.current.querySelectorAll('div');
+        pages[0].classList.add(ourStoryStyle.pageActive);
+        const handlePage = () => {
+            const PageNumber = Math.floor(storyContainerCopy.scrollLeft / storyContainerCopy.offsetWidth);
+            pages[PageNumber].classList.add(ourStoryStyle.pageActive);
+            pages[currentPage].classList.remove(ourStoryStyle.pageActive);
+            currentPage = PageNumber;
+        }
+        storyContainerCopy.addEventListener('scrollend',handlePage);
+        return () => {
+            storyContainerCopy.removeEventListener('scroll',handlePage);
+        }
+    },[]);
     return(
         <>
         <section id='home' className={ourStoryStyle.OurStory}>
-            <div id='moreContentsContainer' className={ourStoryStyle.storyContainer}>
+            <div ref={storyContainer} className={ourStoryStyle.storyContainer}>
                 {
                     contentArray.map((obj)=>(
                         <StoryFrame key={obj.id} obj={obj}/>
                     ))
                 }
             </div>
+            <div ref={pagesContainer} className={ourStoryStyle.pageIndicator}>{pageIndicator()}</div>
         </section>
         </>
     );
